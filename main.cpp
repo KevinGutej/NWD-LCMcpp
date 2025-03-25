f2c21d016e9f6d365c37c32616ec75b144ae3bb0
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2025-03-25 22:57:53 +0100

    Added More Efficient GCD, Better User Experience

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+int findGreatestCommonDivisor(int firstNumber, int secondNumber) {
+    while (firstNumber != secondNumber) {
+        if (firstNumber > secondNumber) {
+            firstNumber -= secondNumber;
+        } else {
+            secondNumber -= firstNumber;
+        }
+    }
+    return firstNumber;
+}
+
 int findGreatestCommonDivisorOld(int firstNumber, int secondNumber) {
     while (secondNumber != 0) {
         int temporaryValue = firstNumber;
@@ -14,48 +25,52 @@ int findGreatestCommonDivisorOld(int firstNumber, int secondNumber) {
     return firstNumber;
 }
 
+int findLeastCommonMultiple(int firstNumber, int secondNumber) {
+    int maxVal = (firstNumber > secondNumber) ? firstNumber : secondNumber;
+    int step = maxVal;
+    while (true) {
+        if (maxVal % firstNumber == 0 && maxVal % secondNumber == 0) {
+            return maxVal;
+        }
+        maxVal += step;
+    }
+}
+
 int findLeastCommonMultipleOld(int firstNumber, int secondNumber) {
     int largestValue = (firstNumber > secondNumber) ? firstNumber : secondNumber;
     while (true) {
-        if (largestValue % firstNumber == 0 && largestValue % secondNumber == 0) {
-            return largestValue;
+        int temp = largestValue;
+        while (temp >= firstNumber) {
+            temp -= firstNumber;
+        }
+        if (temp == 0) {
+            temp = largestValue;
+            while (temp >= secondNumber) {
+                temp -= secondNumber;
+            }
+            if (temp == 0) {
+                return largestValue;
+            }
         }
         largestValue++;
     }
 }
 
-int findGreatestCommonDivisor(int firstNumber, int secondNumber) {
-    while (secondNumber != 0) {
-        int temp = secondNumber;
-        secondNumber = firstNumber % secondNumber;
-        firstNumber = temp;
-    }
-    return firstNumber;
-}
-
-int findLeastCommonMultiple(int firstNumber, int secondNumber) {
-    return (firstNumber * secondNumber) / findGreatestCommonDivisor(firstNumber, secondNumber);
-}
-
 int main() {
     int firstNumber, secondNumber;
 
-    cout << "Enter two numbers: ";
+    cout << "Enter two positive numbers: ";
     cin >> firstNumber >> secondNumber;
 
     if (firstNumber <= 0 || secondNumber <= 0) {
-        cout << "Please enter positive non-zero numbers." << endl;
+        cout << "Error: Please enter positive, non-zero numbers." << endl;
         return 1;
     }
 
     cout << "Old Greatest Common Divisor: " << findGreatestCommonDivisorOld(firstNumber, secondNumber) << endl;
+    cout << "New Greatest Common Divisor: " << findGreatestCommonDivisor(firstNumber, secondNumber) << endl;
     cout << "Old Least Common Multiple: " << findLeastCommonMultipleOld(firstNumber, secondNumber) << endl;
-
-    int gcd = findGreatestCommonDivisor(firstNumber, secondNumber);
-    int lcm = findLeastCommonMultiple(firstNumber, secondNumber);
-
-    cout << "New Greatest Common Divisor: " << gcd << endl;
-    cout << "New Least Common Multiple: " << lcm << endl;
+    cout << "New Least Common Multiple: " << findLeastCommonMultiple(firstNumber, secondNumber) << endl;
 
     return 0;
 }
